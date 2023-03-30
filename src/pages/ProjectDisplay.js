4dc815cc4060393791384cae3475f66efa76f46e
LLChen <qpalzm126@gmail.com>
2023-03-31 01:04:29 +0800

    20230331 0104 material-ui /iconremoved

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
 import { ProjectList } from "../helpers/ProjectList";
-import GitHubIcon from "@material-ui/icons/GitHub";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import "../styles/ProjectDisplay.css";
 
 function ProjectDisplay() {
@@ -11,7 +11,7 @@ function ProjectDisplay() {
     <div className="project">
       <h1> {project.name}</h1>
       <Link to={{ pathname: "https://clone-6cf47.web.app/" }} target="_blank">
-        <img src={project.image} />
+        <img src={project.image} alt="" />
       </Link>
       <p>
         <b>Skills:</b> {project.skills}
