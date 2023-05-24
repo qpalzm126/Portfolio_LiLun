3f0738afbd5ebc97d65fbd3ad5944808d624b7be
LLChen <qpalzm126@gmail.com>
2023-05-24 20:56:48 +0800

    20230524 2057

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,12 @@
 import React from "react";
-import InstagramIcon from "@mui/icons-material/Instagram";
 import "../styles/Footer.css";
 
 function Footer() {
-  return (
-    <div className="footer">
-      {/* <div className="socialMedia">
-        <InstagramIcon />
-      </div> */}
-      <p>&copy; 2023 Lilun Portfolio</p>
-    </div>
-  );
+    return (
+        <div className="footer">
+            <p>&copy; 2023 Lilun Portfolio</p>
+        </div>
+    );
 }
 
 export default Footer;
--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -1,24 +1,22 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
 import { ProjectList } from "../helpers/ProjectList";
-import GitHubIcon from "@mui/icons-material/GitHub";
 import "../styles/ProjectDisplay.css";
 
 function ProjectDisplay() {
-  const { id } = useParams();
-  const project = ProjectList[id];
-  return (
-    <div className="project">
-      <h1> {project.name}</h1>
-      <Link to={{ pathname: project.link }} target="_blank">
-        <img src={project.image} alt="" />
-      </Link>
-      <p>
-        <b>Skills:</b> {project.skills}
-      </p>
-      {/* <GitHubIcon /> */}
-    </div>
-  );
+    const { id } = useParams();
+    const project = ProjectList[id];
+    return (
+        <div className="project">
+            <h1> {project.name}</h1>
+            <Link to={{ pathname: project.link }} target="_blank">
+                <img src={project.image} alt="" />
+            </Link>
+            <p>
+                <b>Skills:</b> {project.skills}
+            </p>
+        </div>
+    );
 }
 
 export default ProjectDisplay;
